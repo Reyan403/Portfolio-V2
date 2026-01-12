a5dab24e8ddf6872694e21b63b16d4d6dd0d24ce
Reyan Ghazzaoui <ghazzaoui.reyan@gmail.com>
2026-01-13 00:01:52 +0100

    finalisation du footer et de la page d'accueil

--- assets/app.js
+++ assets/app.js
@@ -33,8 +33,8 @@ const header = document.querySelector('header');
 function modeSombre() {
     document.body.classList.add('bg-black');
     document.body.classList.add('text-white');
-    header.classList.remove('bg-white'); // Enlève le blanc
-    header.classList.add('shadow-lg', 'shadow-white/50', 'bg-black'); // Ajoute le noir
+    header.classList.remove('bg-white'); 
+    header.classList.add('shadow-lg', 'shadow-white/50', 'bg-black'); 
     theme.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"/>';
     localStorage.setItem('theme', 'sombre');
     if (btnMobile) btnMobile.textContent = "Mode clair";
@@ -56,8 +56,8 @@ function modeSombre() {
 function modeClair() {
     document.body.classList.remove('bg-black');
     document.body.classList.remove('text-white');
-    header.classList.remove('shadow-lg', 'shadow-white/50', 'bg-black'); // Enlève le noir
-    header.classList.add('bg-white'); // Remet le blanc
+    header.classList.remove('shadow-lg', 'shadow-white/50', 'bg-black'); 
+    header.classList.add('bg-white'); 
     theme.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"/>';
     localStorage.setItem('theme', 'clair'); 
     if (btnMobile) btnMobile.textContent = "Mode sombre";
@@ -97,9 +97,7 @@ if(localStorage.getItem('theme') === 'sombre') {
 const menuLinks = document.querySelectorAll('.menu-link');
 menuLinks.forEach(link => {
     link.addEventListener('click', function() {
-        // retirer le soulignement de tous les liens
         menuLinks.forEach(l => l.classList.remove('active-link'));
-        // ajouter le soulignement au lien cliqué
         this.classList.add('active-link');
     });
 });
